3c6d841600cae16be6cb1a65d923efdd44f19e40
hmeshram21 <harsh.meshram.ext@holcim.com>
2025-07-17 15:53:43 +0530

    changes done update one is left

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,57 @@
 const User = require("../model/user.model");
 
-exports.createUser = async (req, resp) => {
+exports.createUser = async (req, res) => {
   try {
+    // Check if userId or email already exists 
+    const existingUserId = await User.findOne({ userId: req.body.userId });
+    if (existingUserId) {
+      return res.status(400).json({ message: "UserId already exists" });
+    }
+    const existingEmail = await User.findOne({ email: req.body.email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
     const user = await User.create(req.body);
-    resp.status(201).json({ message: "User created successfully", user });
+    res.status(201).json({ message: "User created successfully", user });
   } catch (error) {
-    resp.status(500).json({ message: "Failed to create user", error: error.message });
+    res.status(500).json({ message: "Failed to create user", error: error.message });
   }
 };
+
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password"); // exclude password from output
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch users", error: error.message });
+  }
+};
+
+
+// Get a user by userId
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findOne({ userId: req.params.userId }).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error: error.message });
+  }
+};
+
+
+// Delete user by userId
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ userId: req.params.userId }).select("-password");;
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully" , userInfo : user });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete user", error: error.message });
+  }
+};
+
+
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { createUser } = require("../controllers/user.controller");
+const { createUser,getAllUsers, getUserById, deleteUser } = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:userId", getUserById);
+router.delete("/:userId", deleteUser);
+
+
 
 module.exports = router;
